3cd27929a4b80b20eab1117f846cbc58daf810e8
shukshhhhh <helena20020901@gmail.com>
2024-10-20 17:03:27 +0300

    Add files via upload

---
+++ Лабораторная работа 1/task_2.py
@@ -0,0 +1,14 @@
+# TODO Найдите количество книг, которое можно разместить на дискете
+symbols_in_line = 25
+lines_on_page = 50
+pages_in_book = 100
+
+total_number_of_symbols = (symbols_in_line * lines_on_page) * pages_in_book
+
+one_book_weights = total_number_of_symbols * 0.004
+
+kbites_in_disk = 1.44 * 1024
+
+answer = int(kbites_in_disk // one_book_weights)
+
+print("Количество книг, помещающихся на дискету:", answer)
